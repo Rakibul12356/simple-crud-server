d48fcaf49b236abd0a06a352d3dba04bbcf19013
Rakibul12356 <mdrakibulhasan12346@gmail.com>
2025-02-15 11:39:26 +0600

    single  data load function

--- index.js
+++ index.js
@@ -32,6 +32,13 @@ async function run() {
         res.send(result)
     })
 
+    app.get('/users/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id) }
+      const user = await usersCollection.findOne(query);
+      res.send(user)
+    })
+
     app.post('/users',async(req,res)=>{
         const user = req.body;
         console.log('new user',user)
